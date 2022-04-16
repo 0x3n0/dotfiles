77113955021de469a74b34d22acd4d520b229950
0x3n0 <76135196+0x3n0@users.noreply.github.com>
2022-04-16 21:04:42 +0900

    Update blocks.h

--- .config/dwm/dwmblocks/blocks.h
+++ .config/dwm/dwmblocks/blocks.h
@@ -1,19 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
-    {"  ", "/home/sec/.config/dwm/dwmblocks/scripts/kernel",		    360,		        2},
+    {"  ", "$HOME/.config/dwm/dwmblocks/scripts/kernel",		    360,		        2},
 
-    {"  ", "/home/sec/.config/dwm/dwmblocks/scripts/wifi",		        360,	            0},
+    {"  ", "$HOME/.config/dwm/dwmblocks/scripts/wifi",		        360,	            0},
 
-	{" ﮮ ", "/home/sec/.config/dwm/dwmblocks/scripts/upt",		        60,		            2},
+	{" ﮮ ", "$HOME/.config/dwm/dwmblocks/scripts/upt",		        60,		            2},
 
-	{"  ", "/home/sec/.config/dwm/dwmblocks/scripts/pacupdate",		360,		        9},
+	{"  ", "$HOME/.config/dwm/dwmblocks/scripts/pacupdate",		360,		        9},
 	
-	{"  ", "/home/sec/.config/dwm/dwmblocks/scripts/memory",	        6,		            1},
+	{"  ", "$HOME/.config/dwm/dwmblocks/scripts/memory",	        6,		            1},
 
-	{"  ", "/home/sec/.config/dwm/dwmblocks/scripts/volume",			2,		            10},
+	{"  ", "$HOME/.config/dwm/dwmblocks/scripts/volume",			2,		            10},
 
-	{"  ", "/home/sec/.config/dwm/dwmblocks/scripts/clock",			5,		            0},
+	{"  ", "$HOME/.config/dwm/dwmblocks/scripts/clock",			5,		            0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
