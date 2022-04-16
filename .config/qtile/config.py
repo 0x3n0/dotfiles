50002407c1f2cf4949f918925ebaaaa60840b329
0x3n0 <76135196+0x3n0@users.noreply.github.com>
2022-04-16 22:41:55 +0900

    Update config.py

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -129,7 +129,45 @@ keys = [
          Key([mod, "shift"], "space",
              lazy.layout.toggle_split(),
              desc='Toggle between split and unsplit sides of stack'
-             )
+             ),
+         # Dmenu scripts launched using the key chord SUPER+p followed by 'key'
+         KeyChord([mod], "p", [
+             Key([], "e",
+                 lazy.spawn("./dmscripts/dm-confedit"),
+                 desc='Choose a config file to edit'
+                 ),
+             Key([], "i",
+                 lazy.spawn("./dmscripts/dm-maim"),
+                 desc='Take screenshots via dmenu'
+                 ),
+             Key([], "k",
+                 lazy.spawn("./dmscripts/dm-kill"),
+                 desc='Kill processes via dmenu'
+                 ),
+             Key([], "l",
+                 lazy.spawn("./dmscripts/dm-logout"),
+                 desc='A logout menu'
+                 ),
+             Key([], "m",
+                 lazy.spawn("./dmscripts/dm-man"),
+                 desc='Search manpages in dmenu'
+                 ),
+             Key([], "o",
+                 lazy.spawn("./dmscripts/dm-bookman"),
+                 desc='Search your qutebrowser bookmarks and quickmarks'
+                 ),
+             Key([], "r",
+                 lazy.spawn("./dmscripts/dm-reddit"),
+                 desc='Search reddit via dmenu'
+                 ),
+             Key([], "s",
+                 lazy.spawn("./dmscripts/dm-websearch"),
+                 desc='Search various search engines via dmenu'
+                 ),
+             Key([], "p",
+                 lazy.spawn("passmenu"),
+                 desc='Retrieve passwords with dmenu'
+                 )         
          ])
 ]
                
