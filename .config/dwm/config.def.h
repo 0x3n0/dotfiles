4bdf7229c57c39ad0c606ca859470740742bb916
0x3n0 <76135196+0x3n0@users.noreply.github.com>
2022-04-26 18:12:21 +0900

    Update and rename back_config.h to config.def.h

--- .config/dwm/config.def.h
+++ .config/dwm/config.def.h
@@ -1,36 +1,38 @@
-/*  ____ _____  */
-/* |  _ \_   _|  Derek Taylor (DistroTube) */
-/* | | | || |  	http://www.youtube.com/c/DistroTube */
-/* | |_| || |  	http://www.gitlab.com/dwt1/ */
-/* |____/ |_|  	*/ 
-
-/* See LICENSE file for copyright and license details. */
-/* appearance */
 static const unsigned int borderpx = 2;   /* border pixel of windows */
 static const unsigned int snap     = 32;  /* snap pixel */
-static const unsigned int gappx    = 6;   /* pixel gap between clients */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const unsigned int gappx    = 1;   /* pixel gap between clients */
 static const int showbar           = 1;   /* 0 means no bar */
 static const int topbar            = 1;   /* 0 means bottom bar */
-static const int horizpadbar       = 1;   /* horizontal padding for statusbar */
-static const int vertpadbar        = 2;   /* vertical padding for statusbar */
+static const int horizpadbar       = 0;   /* horizontal padding for statusbar */
+static const int vertpadbar        = 0;   /* vertical padding for statusbar */
 /* Mononoki Nerd Font must be installed from AUR nerd-fonts-complete.
  * Otherwise, your default font will be Hack which is found in the standard
- * Arch repos and is listed as a dependency for this build. JoyPixels is also
+ * Arch repos and is listed as a dependency for this build. JoyPixels is also:while
  * a hard dependency and makes colored fonts and emojis possible.
  */
-static const char *fonts[]     = {"monospace:size=9", "fontawesome:size=10", "Nerd Font Mono:weight=bold:size=8:antialias=true:hinting=true"
-						     	};
-static const char col_1[]  = "#282c34"; /* background color of bar */
-static const char col_2[]  = "#282c34"; /* border color unfocused windows */
-static const char col_3[]  = "#d7d7d7";
-static const char col_4[]  = "#6272a4"; /* border color focused windows and tags */
-/* bar opacity 
+
+/* static const char *fonts[]     = {"SauceCodePro Nerd Font Mono:weight=bold:size=9:antialias=true:hinting=true","SymbolsNerdFont:size=10:antialias=true:autohint=true","NotoColorEmoji:size=11:antialias=true:autohint=true"};
+*/
+
+static const char *fonts[]     = { "SymbolsNerdFont:size=11:antialias=true:autohint=true", "FuraCode Nerd Font:style=Medium:size=9" };
+
+
+static const char col_1[]  = "#44475a"; /* background color of bar */
+static const char col_2[]  = "#44475a"; /* border color unfocused windows */
+static const char col_3[]  = "#f8f8f2";
+static const char col_4[]  = "#815757"; /* border color focused windows and tags  #815757 */
+/* bar opacity
  * 0xff is no transparency.
  * 0xee adds wee bit of transparency.
  * 0xdd adds adds a bit more transparency.
  * Play with the value to get desired transparency.
  */
-static const unsigned int baralpha    = 0xff; 
+static const unsigned int baralpha    = 0xff;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]        = {
 	/*               fg         bg         border   */
@@ -45,8 +47,8 @@ static const unsigned int alphas[][3] = {
 
 /* tagging */
 /* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
-/* static const char *tags[] = { "dev", "www", "sys", "doc", "vbox", "chat", "mus", "vid", "gfx" }; */
+/* static const char *tags[] = { "", "", "", "", "", "", "", "", "" }; */
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -89,14 +91,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 /* If you are using the standard dmenu program, use the following. */
 static const char *dmenucmd[]    = { "dmenu_run", "-p", "Run: ", NULL };
-/* If you are using the dmenu-distrotube-git program, use the following for a cooler dmenu! */
-/* static const char *dmenucmd[]    = { "dmenu_run", "-g", "10", "-l", "48", "-p", "Run: ", NULL }; */
 
+/* If you are using the dmenu-distrotube-git program, use the following for a cooler dmenu! */
+/* static const char *dmenucmd[]    = { "dmenu_run", "-g", "10", "-l", "48", "-p", "Run: ", NULL };
+*/
 /* the st terminal with tabbed */
-static const char *termcmd[]     = { "alacritty", NULL };
+static const char *termcmd[]     = { "st", NULL };
 /* An alternative way to launch st along with the fish shell */
 /* static const char *termcmd[]     = { "st", "-e fish", NULL }; */
-static const char *tabtermcmd[]  = { "tabbed", "-r", "2", "alacritty", "-w", "''", NULL };
+static const char *tabtermcmd[]  = { "tabbed", "-r", "2", "st", "-w", "''", NULL };
 
 static Key keys[] = {
 	/* modifier             chain key  key        function        argument */
@@ -112,9 +115,28 @@ static Key keys[] = {
 	{ MODKEY,               -1,        XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,               -1,        XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,               -1,        XK_l,      setmfact,       {.f = +0.05} },
+	
+	{ MODKEY|Mod4Mask,       -1,      	XK_h,      incrgaps,       {.i = +1 } },
+        { MODKEY|Mod4Mask,       -1,       	XK_l,      incrgaps,       {.i = -1 } },
+        { MODKEY|Mod4Mask|ShiftMask, -1,	XK_h,      incrogaps,      {.i = +1 } },
+        { MODKEY|Mod4Mask|ShiftMask,    -1,	XK_l,      incrogaps,      {.i = -1 } },
+        { MODKEY|Mod4Mask|ControlMask,  -1,	XK_h,      incrigaps,      {.i = +1 } },
+        { MODKEY|Mod4Mask|ControlMask,  -1,	XK_l,      incrigaps,      {.i = -1 } },
+        { MODKEY|Mod4Mask,              -1,	XK_0,      togglegaps,     {0} },
+        { MODKEY|Mod4Mask|ShiftMask,    -1,	XK_0,      defaultgaps,    {0} },
+        { MODKEY,                       -1,	XK_y,      incrihgaps,     {.i = +1 } },
+        { MODKEY,                       -1,	XK_o,      incrihgaps,     {.i = -1 } },
+        { MODKEY|ControlMask,           -1,	XK_y,      incrivgaps,     {.i = +1 } },
+        { MODKEY|ControlMask,           -1,	XK_o,      incrivgaps,     {.i = -1 } },
+        { MODKEY|Mod4Mask,              -1,	XK_y,      incrohgaps,     {.i = +1 } },
+        { MODKEY|Mod4Mask,              -1,	XK_o,      incrohgaps,     {.i = -1 } },
+        { MODKEY|ShiftMask,             -1,	XK_y,      incrovgaps,     {.i = +1 } },
+        { MODKEY|ShiftMask,             -1,	XK_o,      incrovgaps,     {.i = -1 } },
+
 	{ MODKEY|ControlMask,   -1,        XK_Return, zoom,           {0} },
 	{ MODKEY,               -1,        XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,     -1,        XK_c,      killclient,     {0} },
+
 
     /* Layout manipulation */
 	{ MODKEY,               -1,        XK_Tab,    cyclelayout,    {.i = -1 } },
@@ -139,20 +161,9 @@ static Key keys[] = {
 	
     /* Keybindings for programs using the format SUPER + ALT + "key" */
 	{ MODKEY|Mod1Mask,      -1,        XK_s,      spawn,          CMD("tabbed -r 2 surf -pe x '.surf/html/homepage.html'") },
-	{ MODKEY|Mod1Mask,      -1,        XK_b,      spawn,          CMD("brave") },
+	{ MODKEY|Mod1Mask,      -1,        XK_b,      spawn,          CMD("chromium") },
 	{ MODKEY|Mod1Mask,      -1,        XK_m,      spawn,          CMD("mailspring") },
-	{ MODKEY|Mod1Mask,      -1,        XK_f,      spawn,          CMD("pcmanfm") },
-	
-    /* Dmenu scripts launched with emacs-style keychords SUPER + p followed by "key" */
-	{ MODKEY,               XK_p,      XK_e,      spawn,          CMD("./dmscripts/dm-confedit") },
-	{ MODKEY,               XK_p,      XK_i,      spawn,          CMD("./dmscripts/dm-maim") },
-	{ MODKEY,               XK_p,      XK_k,      spawn,          CMD("./dmscripts/dm-kill") },
-	{ MODKEY,               XK_p,      XK_l,      spawn,          CMD("./dmscripts/dm-logout") },
-	{ MODKEY,               XK_p,      XK_m,      spawn,          CMD("./dmscripts/dm-man") },
-	{ MODKEY,               XK_p,      XK_r,      spawn,          CMD("./dmscripts/dm-reddit") },
-	{ MODKEY,               XK_p,      XK_s,      spawn,          CMD("./dmscripts/dm-websearch") },
-	{ MODKEY,               XK_p,      XK_p,      spawn,          CMD("passmenu") },
-    
+	{ MODKEY|Mod1Mask,      -1,        XK_f,      spawn,          CMD("pcmanfm") },	
 
 
 	TAGKEYS(                -1,        XK_1,                      0)
@@ -165,7 +176,7 @@ static Key keys[] = {
 	TAGKEYS(                -1,        XK_8,                      7)
 	TAGKEYS(                -1,        XK_9,                      8)
 	{ MODKEY|ShiftMask,     -1,        XK_q,	  quit,		      {0} },
-    { MODKEY|ShiftMask,     -1,        XK_r,      quit,           {1} }, 
+    { MODKEY|ShiftMask,     -1,        XK_r,      quit,           {1} },
 };
 
 /* button definitions */
@@ -184,5 +195,3 @@ static Button buttons[] = {
 	{ ClkTagBar,       MODKEY,      Button1,        tag,            {0} },
 	{ ClkTagBar,       MODKEY,      Button3,        toggletag,      {0} },
 };
-
-
