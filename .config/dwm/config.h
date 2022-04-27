5aed4bf1ddbce634b0d73a99f1319c79864c416e
0x3n0 <76135196+0x3n0@users.noreply.github.com>
2022-04-27 18:10:02 +0900

    Update config.h

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -131,8 +131,8 @@ static Key keys[] = {
 	
     /* Keybindings for programs using the format SUPER + ALT + "key" */
 	{ MODKEY|Mod1Mask,      -1,        XK_s,      spawn,          CMD("tabbed -r 2 surf -pe x '.surf/html/homepage.html'") },
-	{ MODKEY|Mod1Mask,      -1,        XK_b,      spawn,          CMD("brave") },
-	{ MODKEY|Mod1Mask,      -1,        XK_m,      spawn,          CMD("mailspring") },
+	{ MODKEY|Mod1Mask,      -1,        XK_b,      spawn,          CMD("chromium") },
+	{ MODKEY|Mod1Mask,      -1,        XK_t,      spawn,          CMD("telegram") },
 	{ MODKEY|Mod1Mask,      -1,        XK_f,      spawn,          CMD("pcmanfm") },
 
 	TAGKEYS(                -1,        XK_1,                      0)
